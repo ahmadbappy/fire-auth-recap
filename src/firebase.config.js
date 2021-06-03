c6ad4f26ec9ab91a5b2fb52764225a5cc5bf6b1a
Ahmad Bappy <athb.001@gmail.com>
2021-06-03 16:02:49 +0600

    auth recap

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCII2ro5AYmfWjgy-VbzTVaqrkH3pmA068",
+    authDomain: "fire-auth-recap-demo.firebaseapp.com",
+    projectId: "fire-auth-recap-demo",
+    storageBucket: "fire-auth-recap-demo.appspot.com",
+    messagingSenderId: "532740611099",
+    appId: "1:532740611099:web:20d230d3fae99cabad6cbe"
+};
+
+export default firebaseConfig;
